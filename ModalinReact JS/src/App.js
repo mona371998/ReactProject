044fbfd21daf180d646796cd5813da75d8847f48
mona371998 <54792280+mona371998@users.noreply.github.com>
2023-08-30 23:34:50 +0530

    Add files via upload

---
+++ ModalinReact JS/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Model from './Components/Modal';
+
+function App() {
+  return (
+    <div className="App">
+     <Model/>
+    </div>
+  );
+}
+
+export default App;
